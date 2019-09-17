c9b86bfd90fe393f5548d9d64739bfc854f06be0
Alumno Ulsa <alumno@ulsa.ulsa-noroeste.edu.mx>
2019-09-17 11:37:02 -0700

    Agregar archivos de proyecto.

---
+++ GraficadorSeñales/MainWindow.xaml.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GraficadorSeñales
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            
+        }
+
+        private void BtnGraficar_Click(object sender, RoutedEventArgs e)
+        {
+            
+            double tiempoInicial =
+                double.Parse(txtTiempoInicial.Text);
+            double tiempoFinal =
+                double.Parse(txtTiempoFinal.Text);
+            double frecuenciaMuestreo =
+                double.Parse(txtFrecuenciaMuestreo.Text);
+
+
+            Señal señal;
+
+            switch(cbTipoSeñal.SelectedIndex)
+            {
+                case 0: // Parabolica
+                    señal = new SeñalParabolica();
+                
+                
+                    break;
+                case 1: //Senoidal
+                    double amplitud =
+                        double.Parse(
+                    ((ConfiguracionSeñalSenoidal)
+                        (panelConfiguracion.Children[0])).txtAmplitud.Text);
+                    double fase =
+                        double.Parse(
+                            ((ConfiguracionSeñalSenoidal)
+                            (panelConfiguracion.Children[0])).txtFase.Text
+                            );
+                    double frecuencia =
+                        double.Parse(
+                            ((ConfiguracionSeñalSenoidal)
+                            (panelConfiguracion.Children[0])).txtFrecuencia.Text
+                            );
+                    señal = 
+                        new SeñalSenoidal(amplitud, fase, frecuencia);
+                    
+                    break;
+                case 2:
+                    string rutaArchivo =
+                        ((ConfiguracionAudio)
+                        (panelConfiguracion.Children[0])).txtRutaArchivo.Text;
+                    señal = new SeñalAudio(rutaArchivo);
+                    break;
+                default:
+                    señal = null;
+                    break;
+            }
+
+            if (cbTipoSeñal.SelectedIndex != 2 
+                && señal != null)
+            {
+                señal.TiempoInicial =
+                        tiempoInicial;
+                señal.TiempoFinal =
+                    tiempoFinal;
+                señal.FrecuenciaMuestreo =
+                    frecuenciaMuestreo;
+
+                señal.construirSeñal();
+            }
+
+            
+
+            double amplitudMaxima = 
+                señal.AmplitudMaxima;
+
+            plnGrafica.Points.Clear();
+
+
+            foreach (Muestra muestra in señal.Muestras)
+            {
+                plnGrafica.Points.Add(
+                    adaptarCoordenadas(muestra.X,
+                    muestra.Y, tiempoInicial, amplitudMaxima)
+                    );
+            }
+
+
+            lblLimiteSuperior.Text =
+                amplitudMaxima.ToString("F");
+            lblLimiteInferior.Text =
+                "-" + amplitudMaxima.ToString("F");
+
+            plnEjeX.Points.Clear();
+            plnEjeX.Points.Add(
+                adaptarCoordenadas(tiempoInicial, 0.0,
+                tiempoInicial, amplitudMaxima)
+                );
+            plnEjeX.Points.Add(
+                adaptarCoordenadas(tiempoFinal, 0.0, tiempoInicial,
+                amplitudMaxima)
+                );
+
+            plnEjeY.Points.Clear();
+            plnEjeY.Points.Add(
+                adaptarCoordenadas(0.0, amplitudMaxima,
+                tiempoInicial,amplitudMaxima));
+            plnEjeY.Points.Add(
+                adaptarCoordenadas(0.0, -amplitudMaxima,
+                tiempoInicial, amplitudMaxima)) ;
+           
+
+        }
+
+        public Point adaptarCoordenadas(
+            double x, double y, double tiempoInicial,
+            double amplitudMaxima)
+        {
+            return new Point((x - tiempoInicial) * scrGrafica.Width,
+                (-1 * (
+                y * ((( scrGrafica.Height  / 2.0  ) -25 ) / amplitudMaxima) )) +
+                (scrGrafica.Height / 2.0) );
+        }
+
+        private void CbTipoSeñal_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            panelConfiguracion.Children.Clear();
+            switch(cbTipoSeñal.SelectedIndex)
+            {
+                case 0: //Parabolica
+                    break;
+                case 1: //Senoidal
+                    panelConfiguracion.Children.Add(
+                        new ConfiguracionSeñalSenoidal());
+                    break;
+                case 2:
+                    panelConfiguracion.Children.Add(
+                        new ConfiguracionAudio()
+                        ) ;
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
